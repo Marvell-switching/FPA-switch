a124e4317f001b5fe56fd0daf6d874f29ba6c65d
Vitaly.Vovnoboy <vitalyv@marvell.com>
2018-01-03 16:41:38 +0200

    Update make_deb.sh

--- scripts/make_deb.sh
+++ scripts/make_deb.sh
@@ -13,15 +13,15 @@ cp lib/libfpa.so $LIB_NAME/usr/lib/
 
 if [ ! -d "$LIB_NAME/DEBIAN" ]; then
         mkdir -p $LIB_NAME/DEBIAN
-        echo "package: mrvllibfpa"                          >  $LIB_NAME/DEBIAN/control
+        echo "package: mrvllibfpa"                          >> $LIB_NAME/DEBIAN/control
         echo "Version: $2"                                  >> $LIB_NAME/DEBIAN/control
         echo "Section: base"                                >> $LIB_NAME/DEBIAN/control
         echo "Priority: optional"                           >> $LIB_NAME/DEBIAN/control
         echo "Architecture: amd64"                          >> $LIB_NAME/DEBIAN/control
         echo "Depends:"                                     >> $LIB_NAME/DEBIAN/control
-        echo "Maintainer: Vladimir Kuk <vkuk@marvell.com>"  >> $LIB_NAME/DEBIAN/control
+        echo "Maintainer: Ziv Erlichson <zive@marvell.com>" >> $LIB_NAME/DEBIAN/control
         echo "Description: Marvell FPA driver"              >> $LIB_NAME/DEBIAN/control
-        echo " based on FPA v$3"                            >> $LIB_NAME/DEBIAN/control
+        echo "based on FPA v$3"                             >> $LIB_NAME/DEBIAN/control
 fi
 dpkg-deb --build $LIB_NAME/
 popd > /dev/null
